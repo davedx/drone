c53b2d311c71d812a3c830d888693e883ffb2273
Dave Clayton <davedx@gmail.com>
2013-07-10 16:54:36 +0200

    Initial commit and documentation

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
-drone
+Drone
 =====
 
-Small, simple remote BDD testing designed for target environments
+Drone is a small, simple remote BDD testing tool designed for target environments.
+
+There are plenty of existing test frameworks and runners out there, but figuring out how to get them working can be a bit of a headache: you may or may not end up wrestling with building, dependencies, bad documentation or simply drowning in too many options, features and jargon. Drone aims to satisfy a core set of requirements, following the KISS and YAGNI mantras:
+
+* Support out-of-the-box testing in target environments, e.g. embedded or virtualized browsers
+* Support a minimal set of BDD declarations in a fairly typical style (describe, it, assert, before, beforeEach)
+* Minimum mental load configuration or build process
+* Remote or console.log based reporting
+* Simple async/sync support: functions with a callback parameter are treated as async; functions without aren't
+
+Configuration
+=============
+
+1. Include Runner.js
+2. Include your test specs (see below for examples)
+3. Invoke Runner.run() directly or use a keypress trigger (see bottom of Runner.js for examples)
+
+Example: synchronous test (suited to unit tests)
+
+```
+describe("The cat module stores registration numbers", function () {
+	var cat = null;
+	before(function () {
+		cat = Cats.create();
+	});
+
+	it('Should store correctly calculated registration numbers', function () {
+		cat.save('Bob', 123);
+		assert(cat.registration == 'B123');
+
+		cat.save('John', 456);
+		assert(cat.registration == 'J456');
+	});
+});
+```
+
+Example: async test (suited to integration tests)
+
+```
+describe("Users add products to cart", function () {
+	var cart = null;
+	var productId = '1234';
+
+	before(function (done) {
+		// go to product page
+		app.navigate({action: product, id: productId}, done);
+	});
+
+	it("should add a product to a cart", function (done) {
+		// maybe you can do this, or maybe you use jQuery to fake an click event on an 'add product' button:
+		app.clickButton({id: add}, function() {
+			// maybe there's a webservice call in your frontend that calls this callback when it gets a response
+			assert($('#cart_counter').html() == '1');
+			assert(app.session.cart.count == 1);
+			done();
+		});
+	});
+});
+```
+
---
+++ Runner.js
@@ -0,0 +1,126 @@
+Runner = (function () {
+	var reporterFn = console.log;
+	var specs = [];
+
+	var current = 0;
+	var specFilter = null;
+	var queueIt = [];
+	var queueBefore = null;
+	var queueBeforeEach = null;
+
+	var nextTest = function (q) {
+		if(q.length == 0)
+			return nextSpec();
+		var f = q.shift();
+		if(f.desc)
+			reporterFn(f.desc);
+		if(f.fn.length) {
+			f.fn(function () {
+				nextTest(q);
+			});
+		} else {
+			f.fn();
+			nextTest(q);
+		}
+	}
+
+	var nextSpec = function () {
+		if(specFilter) {
+			while(current < specs.length && !specs[current].desc.match(specFilter)) {
+				current++;
+			}
+		}
+
+		if(current == specs.length) {
+			return;
+		}
+		queueIt = [];
+		queueBefore = null;
+		queueBeforeEach = null;
+		specs[current].fn();
+		// build full queue:
+		var q = [];
+		if(queueBefore)
+			q.push({fn: queueBefore});
+		for(var i in queueIt) {
+			if(queueBeforeEach)
+				q.push({fn: queueBeforeEach});
+			q.push(queueIt[i]);
+		}
+		reporterFn(specs[current].desc);
+		current++;
+		nextTest(q);
+	};
+	return {
+		reporter: function (fn) {
+			reporterFn = fn;
+		},
+		log: function (msg) {
+			reporterFn(msg);
+		},
+		trigger: function (opts) {
+			if(opts.keydown && window) {
+				window.onload = function() {
+				    window.addEventListener("keydown", function (e) {
+				    	if(e.keyCode == opts.keydown)
+				    		Runner.run(opts.filter);
+				    });
+				}
+			}
+		},
+		run: function (filter) {
+			reporterFn("Starting tests with filter: "+filter);
+			current = 0;
+			specFilter = filter;
+			nextSpec();
+		},
+		addSpec: function (desc, fn) {
+			specs.push({fn: fn, desc: desc});
+		},
+		enqueueIt: function (desc, fn) {
+			queueIt.push({fn: fn, desc: desc});
+		},
+		enqueueBefore: function (fn) {
+			queueBefore = fn;
+		},
+		enqueueBeforeEach: function (fn) {
+			queueBeforeEach = fn;
+		}
+	}
+})();
+
+describe = function (desc, fn) {
+	Runner.addSpec(desc, fn);
+}
+
+xdescribe = function (desc, fn) {}
+
+before = function (fn) {
+	Runner.enqueueBefore(fn);
+}
+
+beforeEach = function (fn) {
+	Runner.enqueueBeforeEach(fn);
+}
+
+it = function (desc, fn) {
+	Runner.enqueueIt(desc, fn);
+}
+
+xit = function (desc, fn) {}
+
+assert = function (expression) {
+	if(expression) {
+		Runner.log("#### PASS");
+	} else {
+		Runner.log("#### FAIL");
+	}
+}
+
+/**
+ * Setup example:
+ *
+ * Runner.reporter(MyCustomReportingFunction);
+ * Runner.trigger({keydown: 84}); // 't'
+ *
+ */
