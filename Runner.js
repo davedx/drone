bb392c28c115ffe7c712e2020a3fbe458012badc
Dave Clayton <daclayton@libertyglobal.com>
2013-09-23 08:53:05 +0200

    Added async timeouts. Added failcount and finished callback with number of failed tests. Added phantomjs shim for bind.

--- Runner.js
+++ Runner.js
@@ -1,5 +1,17 @@
+// shim for PhantomJS
+// see: https://github.com/ariya/phantomjs/issues/10522
+Function.prototype.bind = Function.prototype.bind || function (thisp) {
+  var fn = this;
+  return function () {
+    return fn.apply(thisp, arguments);
+  };
+};
+
 Runner = (function () {
 	var reporterFn = console.debug.bind(console);
+	var configuration = {
+		asyncTimeout: 2500
+	};
 	var specs = [];
 
 	var current = 0;
@@ -7,22 +19,27 @@ Runner = (function () {
 	var queueIt = [];
 	var queueBefore = null;
 	var queueBeforeEach = null;
+	var queueAfter = null;
+	var finished = null;
+	var fails = 0;
 
 	var nextTest = function (q) {
-		if(q.length == 0)
+		if(q.length === 0)
 			return nextSpec();
 		var f = q.shift();
 		if(f.desc)
 			reporterFn(f.desc);
 		if(f.fn.length) {
+			var timeout = window.setTimeout(function() { assert('TIMEOUT  ' + f.desc, false); nextTest(q) }, configuration.asyncTimeout);
 			f.fn(function () {
+				window.clearTimeout(timeout);
 				nextTest(q);
 			});
 		} else {
 			f.fn();
 			nextTest(q);
 		}
-	}
+	};
 
 	var nextSpec = function () {
 		if(specFilter) {
@@ -32,11 +49,13 @@ Runner = (function () {
 		}
 
 		if(current == specs.length) {
+			finished && finished(fails);
 			return;
 		}
 		queueIt = [];
 		queueBefore = null;
 		queueBeforeEach = null;
+		queueAfter = null;
 		specs[current].fn();
 		// build full queue:
 		var q = [];
@@ -47,6 +66,8 @@ Runner = (function () {
 				q.push({fn: queueBeforeEach});
 			q.push(queueIt[i]);
 		}
+		if(queueAfter)
+			q.push({fn: queueAfter});
 		reporterFn(specs[current].desc);
 		current++;
 		nextTest(q);
@@ -58,21 +79,27 @@ Runner = (function () {
 		log: function (msg) {
 			reporterFn(msg);
 		},
+		configure: function (cfg) {
+			configuration = cfg;
+		},
 		trigger: function (opts) {
 			if(opts.keydown && window) {
-				window.onload = function() {
-				    window.addEventListener("keydown", function (e) {
-				    	if(e.keyCode == opts.keydown)
-				    		Runner.run(opts.filter);
-				    });
-				}
+				window.addEventListener("keydown", function (e) {
+					if(e.keyCode == opts.keydown) {
+						Runner.run(opts.filter);
+					}
+				});
 			}
 		},
-		run: function (filter) {
-			reporterFn("Starting tests with filter: "+filter);
+		run: function (filter, finishedCallback) {
+			reporterFn("**** Starting tests [filter: "+filter+"] ****");
 			current = 0;
+			finished = finishedCallback;
 			specFilter = filter;
 			nextSpec();
+		},
+		countFails: function () {
+			fails++;
 		},
 		addSpec: function (desc, fn) {
 			specs.push({fn: fn, desc: desc});
@@ -85,42 +112,39 @@ Runner = (function () {
 		},
 		enqueueBeforeEach: function (fn) {
 			queueBeforeEach = fn;
+		},
+		enqueueAfter: function (fn) {
+			queueAfter = fn;
 		}
-	}
+	};
 })();
 
 describe = function (desc, fn) {
 	Runner.addSpec(desc, fn);
-}
+};
 
-xdescribe = function (desc, fn) {}
+xdescribe = function (desc, fn) {};
 
 before = function (fn) {
 	Runner.enqueueBefore(fn);
-}
+};
 
 beforeEach = function (fn) {
 	Runner.enqueueBeforeEach(fn);
-}
+};
+
+after = function (fn) {
+	Runner.enqueueAfter(fn);
+};
 
 it = function (desc, fn) {
 	Runner.enqueueIt(desc, fn);
-}
+};
 
-xit = function (desc, fn) {}
+xit = function (desc, fn) {};
 
-assert = function (expression) {
-	if(expression) {
-		Runner.log("#### PASS");
-	} else {
-		Runner.log("#### FAIL");
-	}
-}
-
-/**
- * Setup example:
- *
- * Runner.reporter(MyCustomReportingFunction);
- * Runner.trigger({keydown: 84}); // 't'
- *
- */
+assert = function (desc, expression) {
+	Runner.log((expression ? "PASS" : "FAIL") + "  " + desc);
+	if(!expression)
+		Runner.countFails();
+};
